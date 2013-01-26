898ae22a88addb1f9b53414b5dfbbab4d7dd2e41
Tristan Straub <tristanstraub@gmail.com>
2013-01-26 20:34:49 +1000

    slight speed up

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -17,11 +17,8 @@ define(['underscore'], function(_) {
     return [a[0]-b[0],a[1]-b[1],a[2]-b[2]];
   };
 
-  var add3 = function(a) {
-    _.each(Array.prototype.slice.call(arguments, 1), function(b) {
-      a = [a[0]+b[0],a[1]+b[1],a[2]+b[2]];
-    });
-    return a;
+  var add3 = function(a, b) {
+    return [a[0]+b[0],a[1]+b[1],a[2]+b[2]];
   };
   
   var scale3 = function(c, x) {
@@ -56,20 +53,8 @@ define(['underscore'], function(_) {
     return o;
   };
 
-  var intersectPlane = function(s, p0, p1, p2) {
-    var k, d1, d2, n, D;
-
-    d1 = diff3(p1, p0);
-    d2 = diff3(p2, p0);
-    n = unit3(cross3(d1,d2));
-
-    k = dot3(p0, n)/dot3(s, n);
-    D = diff3(scale3(k, s), p0);
-
-    var u = dot3(cross3(D, d2), n)/mag3(d1);
-    var v = dot3(cross3(D, d1), n)/mag3(d2);
-
-    return [u, v];
+  var intersectPlane = function(D, d1, magd1, d2, magd2, n) {
+    return [dot3(cross3(D, d2), n)/magd1, dot3(cross3(D, d1), n)/magd2];
   };
 
   var loop2 = function(na, nb, fn) {
@@ -94,7 +79,7 @@ define(['underscore'], function(_) {
     loop2(w,h,function(x,y,dx,dy) {
       var u = x*pw;
       var v = (h-y)*ph;
-      var s = add3(s0, scale3(u, s1), scale3(v, s2));
+      var s = add3(add3(s0, scale3(u, s1)), scale3(v, s2));
 
       fn(x, y, s);
     });
@@ -161,9 +146,6 @@ define(['underscore'], function(_) {
         buffer.data[3+bpos] = 0;
       });
     };
-      
-    //    ctx.putImageData(texture, 0,0, 0,0,texture.width,texture.height);
-
 
     var animFrame = window.requestAnimationFrame ||
           window.webkitRequestAnimationFrame ||
@@ -182,19 +164,24 @@ define(['underscore'], function(_) {
 
     var p0 = [-.5,-1,-3];
 
+
+
     var game_loop = function () {
       clearBuffer();
       var R = axisRotationMatrix3(unit3([0,1,0]), theta);
       var dd1 = mat3mulvec3(R, d1);
       var dd2 = mat3mulvec3(R, d2);
+      var magdd1 = mag3(dd1);
+      var magdd2 = mag3(dd2);
 
-      var p1 = add3(p0, dd1);
-      var p2 = add3(p0, dd2);
+      var n = unit3(cross3(dd1,dd2));
+      var dotp0n = dot3(p0, n);
 
       theta += 0.1;
 
       trace($canvas.width(), $canvas.height(), function(x,y,s) {
-        var uv = intersectPlane(unit3(s), p0, p1, p2);
+        var D = diff3(scale3(dotp0n/dot3(s, n), s), p0);
+        var uv = intersectPlane(D, dd1, magdd1, dd2, magdd2, n);
         if (0 <= uv[0] && uv[0] <= 1 &&
             0 <= uv[1] && uv[1] <= 1) {
           var bpos = (y*buffer.width+x)*4;
